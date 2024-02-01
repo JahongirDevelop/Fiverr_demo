ce14a25cd8e5f27e024cddb72b9a80a1befe4adf
Jahongir <sjahongir22@gmail.com>
2024-02-01 19:05:42 +0500

    change on some entities

--- src/main/java/soqqa/uz/fiverr_demo/entity/Gigs.java
+++ src/main/java/soqqa/uz/fiverr_demo/entity/Gigs.java
@@ -1,4 +1,20 @@
 package soqqa.uz.fiverr_demo.entity;
 
-public class Gigs {
+import jakarta.persistence.Entity;
+import lombok.*;
+import soqqa.uz.fiverr_demo.entity.enums.Category;
+
+@Entity(name = "gigs")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Gigs extends BaseEntity {
+    private String gigTitle;
+    private Category category;
+    private String full_name;
+    private String description;
+    private Double price;
+    private String programmingLanguage;
 }
--- src/main/java/soqqa/uz/fiverr_demo/entity/Payment.java
+++ src/main/java/soqqa/uz/fiverr_demo/entity/Payment.java
@@ -1,6 +1,15 @@
 package soqqa.uz.fiverr_demo.entity;
 
-public class Payment {
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity(name = "payment")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Payment extends BaseEntity {
     private Double amount;
     private Card buyerCard;
     private Card sellerCard;
