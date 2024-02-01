5f7edd61296054de5afc676f9095d10c946e0912
Jahongir <sjahongir22@gmail.com>
2024-02-01 20:19:01 +0500

    change exception package

---
+++ src/main/java/soqqa/uz/fiverr_demo/exception/DataAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package soqqa.uz.fiverr_demo.exception;
+
+public class DataAlreadyExistsException extends RuntimeException {
+    public DataAlreadyExistsException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/soqqa/uz/fiverr_demo/exception/DataNotFoundException.java
@@ -0,0 +1,7 @@
+package soqqa.uz.fiverr_demo.exception;
+
+public class DataNotFoundException extends RuntimeException {
+    public DataNotFoundException(String msg) {
+        super(msg);
+    }
+}
