9ac10928095030f00283034e7b2303717d88720e
Jahongir <sjahongir22@gmail.com>
2024-02-06 20:43:03 +0500

    delete method for all entities

--- src/main/java/soqqa/uz/fiverr_demo/controller/GigsController.java
+++ src/main/java/soqqa/uz/fiverr_demo/controller/GigsController.java
@@ -4,13 +4,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import soqqa.uz.fiverr_demo.dto.request.GigsCreateRequest;
 import soqqa.uz.fiverr_demo.dto.response.GigsResponse;
 import soqqa.uz.fiverr_demo.service.GigsService;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,5 +20,10 @@ public class GigsController {
     @PostMapping("/create-gigs")
     public ResponseEntity<GigsResponse> createGigs(@RequestBody @Valid GigsCreateRequest createRequest){
         return ResponseEntity.status(HttpStatus.CREATED).body(gigsService.create(createRequest));
+    }
+
+    @DeleteMapping("/delete-gigs{gigsId}")
+    public ResponseEntity<String> deleteGigs(@PathVariable UUID gigsId){
+        return ResponseEntity.status(200).body(gigsService.delete(gigsId));
     }
 }
--- src/main/java/soqqa/uz/fiverr_demo/controller/PaymentController.java
+++ src/main/java/soqqa/uz/fiverr_demo/controller/PaymentController.java
@@ -4,13 +4,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import soqqa.uz.fiverr_demo.dto.request.PaymentCreateRequest;
 import soqqa.uz.fiverr_demo.dto.response.PaymentResponse;
 import soqqa.uz.fiverr_demo.service.PaymentService;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,5 +20,10 @@ public class PaymentController {
     @PostMapping("/create-payment")
     public ResponseEntity<PaymentResponse> createPayment(@RequestBody @Valid PaymentCreateRequest createRequest){
         return ResponseEntity.status(HttpStatus.CREATED).body(paymentService.create(createRequest));
+    }
+
+    @DeleteMapping("/delete-payment{paymentId}")
+    public ResponseEntity<String> deletePayment(@PathVariable UUID paymentId){
+        return ResponseEntity.status(200).body(paymentService.delete(paymentId));
     }
 }
