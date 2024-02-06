35e6b1408854069d7878ed752b535bba1ea98c2b
Jahongir <sjahongir22@gmail.com>
2024-02-06 21:27:45 +0500

    added get gigs by category method

--- src/main/java/soqqa/uz/fiverr_demo/controller/GigsController.java
+++ src/main/java/soqqa/uz/fiverr_demo/controller/GigsController.java
@@ -7,8 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import soqqa.uz.fiverr_demo.dto.request.GigsCreateRequest;
 import soqqa.uz.fiverr_demo.dto.response.GigsResponse;
+import soqqa.uz.fiverr_demo.entity.Gigs;
+import soqqa.uz.fiverr_demo.entity.enums.Category;
 import soqqa.uz.fiverr_demo.service.GigsService;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -25,5 +28,13 @@ public class GigsController {
     @DeleteMapping("/delete-gigs{gigsId}")
     public ResponseEntity<String> deleteGigs(@PathVariable UUID gigsId){
         return ResponseEntity.status(200).body(gigsService.delete(gigsId));
+    }
+
+    @GetMapping("/getAllGigsByCategory")
+    public List<GigsResponse> getAllGigsByCategory(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam Category category){
+        return ResponseEntity.status(200).body(gigsService.getAllGigsByCategory(page, size, category)).getBody();
     }
 }
--- src/main/java/soqqa/uz/fiverr_demo/service/GigsService.java
+++ src/main/java/soqqa/uz/fiverr_demo/service/GigsService.java
@@ -2,12 +2,17 @@ package soqqa.uz.fiverr_demo.service;
 
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import soqqa.uz.fiverr_demo.dto.request.GigsCreateRequest;
 import soqqa.uz.fiverr_demo.dto.response.GigsResponse;
 import soqqa.uz.fiverr_demo.entity.Card;
 import soqqa.uz.fiverr_demo.entity.Gigs;
 import soqqa.uz.fiverr_demo.entity.User;
+import soqqa.uz.fiverr_demo.entity.enums.Category;
 import soqqa.uz.fiverr_demo.entity.enums.UserRole;
 import soqqa.uz.fiverr_demo.exception.DataNotFoundException;
 import soqqa.uz.fiverr_demo.repository.GigsRepository;
@@ -58,4 +63,33 @@ public class GigsService {
         return gigsRepository.findById(gigsId)
                 .orElseThrow(() -> new DataNotFoundException("Gigs not found with this id: " + gigsId));
     }
+
+//    public List<GigsResponse> getAllGigsByCategory(int page, int size, Category category) {
+//        Pageable pageable = PageRequest.of(page, size);
+//        List<Gigs> gigs = gigsRepository.findAllByCategory(pageable,category).getContent();
+//        return modelMapper.map(gigs, new TypeToken<List<GigsResponse>>(){}.getType());
+//    }
+
+    public List<GigsResponse> getAllGigsByCategory(int page, int size, Category category) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Gigs> gigsPage = gigsRepository.findAllByCategory(pageable, category);
+        List<Gigs> gigs = gigsPage.getContent();
+
+        List<GigsResponse> gigsResponses = new ArrayList<>();
+
+        for (Gigs gig : gigs) {
+            GigsResponse gigsResponse = GigsResponse.builder()
+                    .user(gig.getUser().getId())
+                    .gigTitle(gig.getGigTitle())
+                    .category(gig.getCategory())
+                    .full_name(gig.getFull_name())
+                    .description(gig.getDescription())
+                    .price(gig.getPrice())
+                    .programmingLanguage(gig.getProgrammingLanguage())
+                    .build();
+            gigsResponses.add(gigsResponse);
+        }
+        return gigsResponses;
+    }
+
 }
