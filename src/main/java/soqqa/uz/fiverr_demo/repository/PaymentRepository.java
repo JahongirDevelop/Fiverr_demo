cf906eda91a054f0d63ad10f8eb6f73832893d9a
Jahongir <sjahongir22@gmail.com>
2024-02-07 21:40:09 +0500

    added search by timestamp  method

--- src/main/java/soqqa/uz/fiverr_demo/repository/PaymentRepository.java
+++ src/main/java/soqqa/uz/fiverr_demo/repository/PaymentRepository.java
@@ -3,8 +3,12 @@ package soqqa.uz.fiverr_demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import soqqa.uz.fiverr_demo.entity.Payment;
+
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
+    Payment searchByTimestamp(LocalDateTime time);
+
 }
