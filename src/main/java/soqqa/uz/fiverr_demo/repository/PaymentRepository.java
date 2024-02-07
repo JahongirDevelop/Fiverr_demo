b94e559e64ba25c1b0dbc36e42eca926bc994e44
Jahongir <sjahongir22@gmail.com>
2024-02-07 21:58:50 +0500

    added search payments between  method

--- src/main/java/soqqa/uz/fiverr_demo/repository/PaymentRepository.java
+++ src/main/java/soqqa/uz/fiverr_demo/repository/PaymentRepository.java
@@ -1,14 +1,22 @@
 package soqqa.uz.fiverr_demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import soqqa.uz.fiverr_demo.entity.Payment;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
-    Payment searchByTimestamp(LocalDateTime time);
+    Payment searchByTimestampAfterAndTimestampBefore(LocalDateTime after, LocalDateTime before);
+//    List<Payment> findByTimestampBetween(LocalDateTime startTime, LocalDateTime endTime);
+
+    @Query("SELECT p FROM payment p WHERE p.timestamp BETWEEN :startTime AND :endTime")
+    List<Payment> findPaymentsBetween(@Param("startTime") LocalDateTime startTime, @Param("endTime") LocalDateTime endTime);
+
 
 }
