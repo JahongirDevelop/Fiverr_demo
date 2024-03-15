d1d80e0ff03dccca6920e78cd28a4449b22e6b4a
JahongirDevelop <sjahongir221@gmail.com>
2024-03-15 14:42:41 +0500

    AuthenticationService

--- src/main/java/soqqa/uz/fiverr_demo/config/jwt/AuthenticationService.java
+++ src/main/java/soqqa/uz/fiverr_demo/config/jwt/AuthenticationService.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Service;
 import java.util.List;
-
 @Service
 public class AuthenticationService {
 
