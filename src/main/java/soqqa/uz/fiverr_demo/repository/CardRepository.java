c45b2bcd88de3c9da8a6253b0b1bdf321ffdb38b
JahongirDevelop <sjahongir221@gmail.com>
2024-03-16 23:06:11 +0500

    CardRepository

--- src/main/java/soqqa/uz/fiverr_demo/repository/CardRepository.java
+++ src/main/java/soqqa/uz/fiverr_demo/repository/CardRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import soqqa.uz.fiverr_demo.entity.Card;
 import soqqa.uz.fiverr_demo.entity.User;
-
 import java.util.UUID;
 @Repository
 public interface CardRepository extends JpaRepository<Card, UUID> {
