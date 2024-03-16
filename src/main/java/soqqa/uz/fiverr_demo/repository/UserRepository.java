fa8056a76b66103d75f768e3431812bb12ea11bf
JahongirDevelop <sjahongir221@gmail.com>
2024-03-16 23:05:29 +0500

    UserRepository

--- src/main/java/soqqa/uz/fiverr_demo/repository/UserRepository.java
+++ src/main/java/soqqa/uz/fiverr_demo/repository/UserRepository.java
@@ -3,11 +3,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import soqqa.uz.fiverr_demo.entity.Gigs;
 import soqqa.uz.fiverr_demo.entity.User;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
